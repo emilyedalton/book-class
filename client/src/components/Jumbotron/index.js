a039252a58f9eff478ca2e65282811b5319fa371
Emily Dalton <emilydalton@dhcp-10-105-61-102.wireless.northwestern.private>
2019-03-13 12:14:41 -0500

    code for inline style in jumbotron component

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")"}}
       className="jumbotron"
     >
       {children}
