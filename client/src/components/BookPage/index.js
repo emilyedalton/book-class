f8e7ad1130782b3f9f0b94968b0aeb3904415f18
Emily Dalton <emilydalton@dhcp-10-105-61-102.wireless.northwestern.private>
2019-03-13 11:38:30 -0500

    adding styling to bookpage

--- client/src/components/BookPage/index.js
+++ client/src/components/BookPage/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'; 
-// import TodoList from './components/TodoList';
-// import './App.css';
+import "../BookPage/style.css"
 
 class BookPage extends Component {
   render(props) {
@@ -8,13 +7,17 @@ class BookPage extends Component {
       <div >
 
 {/* <li>{props.title}</li> */}
+ 
  <h1><a href={this.props.link}>{this.props.title}</a></h1>
  <h2>{this.props.author}</h2>
+ <div id = "description">
  
- <div id = "description"><img src ={this.props.image} alt="book_image"/><p>{this.props.description}</p></div>
-
+ <img src ={this.props.image} alt="book_image"/><p>{this.props.description}</p>
  <button onClick={this.props.onClick} type="button"class="btn btn-dark">Add to My Course Adoptions</button>
 
+ </div>
+<div>
+ </div>
       </div>
     );
   }
