975df1530682ff69b24304c926d6ba4ef25b7aa0
Emily Dalton <emilydalton@dhcp-10-105-61-102.wireless.northwestern.private>
2019-03-16 13:33:14 -0500

    adding northwestern only searches

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -3,9 +3,9 @@ module.exports = {
 
   
 googleFindAll: function(req, res) {
-
+req.query.q=req.query.q+"+inpublisher:Northwestern"
     axios
-    .get("https://www.googleapis.com/books/v1/volumes/",{ params: req.query },"+inpublisher:Northwestern")
+    .get("https://www.googleapis.com/books/v1/volumes/",{ params: req.query })
     .then(results=>results.data.items)
     .then(results=>res.json(results))
     .catch(err => res.status(422).json(err));
